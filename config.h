74184967eaa2bca4a83285dda10d15798c15e739
Sako Hartounian <sakohartounian@yahoo.com>
2021-03-24 11:33:49 -0700

    telegram added to desktop rules

--- config.h
+++ config.h
@@ -56,6 +56,7 @@ static const Rule rules[] = {
 	/* class    		instance      	title			tags mask    	isfloating   isterminal  noswallow  monitor */
 	{ "Gimp",		NULL,		NULL,       	    	1 << 5,       	0,           0,         0,        -1 },
 	{ "discord",		NULL,       	NULL,       	    	1 << 2,       	0,           0,         0,        -1 },
+	{ "telegram-desktop",	NULL,       	NULL,       	    	1 << 2,       	0,           0,         0,        -1 },
 	{ "Chromium",		NULL,       	NULL,       	    	1 << 1,       	0,           0,         0,        -1 },
 	{ "Brave-browser",	NULL,       	NULL,       	    	1 << 1,       	0,           0,         0,        -1 },
 	{ "Steam",		NULL,       	NULL,       	    	1 << 3,       	0,           0,         0,        -1 },
